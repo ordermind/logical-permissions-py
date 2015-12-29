47da98d3a5e9b81d301c10b94ba0484426972667
Kristofer Tengstrom <info@kristofer-tengstrom.se>
2015-12-29 14:04:32 +0100

    kristofer: Updated setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,35 @@
- 
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+  long_description = f.read() 
+
+setup(
+  name = 'logical-permissions',
+  version = '1.0.0',
+  description = 'Provides support for dictionary-based permissions with logic gates such as AND and OR.',
+  long_description = long_description,
+  url = 'https://github.com/Ordermind/logical-permissions-py',
+  author = 'Kristofer Tengström',
+  author_email = 'ordermind@gmail.com',
+  classifiers = [
+    'Development Status :: 5 - Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+  ],
+  keywords = 'permissions',
+  packages = find_packages(exclude=['tests*']),
+)
