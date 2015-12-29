90ee64fbfba2637e9248d27920c267db64cab0bb
Kristofer Tengstrom <info@kristofer-tengstrom.se>
2015-12-29 14:34:51 +0100

    kristofer: Changed version to 1.0.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
   name = 'logical-permissions',
-  version = '1.0.0',
+  version = '1.0.1',
   description = 'Provides support for dictionary-based permissions with logic gates such as AND and OR.',
   long_description = long_description,
   url = 'https://github.com/Ordermind/logical-permissions-py',
