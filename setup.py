0fae156adf87aca7a7508fdc826fe605efe6dcfd
Kristofer Tengstrom <info@kristofer-tengstrom.se>
2016-08-20 17:53:04 +0200

    kristofer: Added MIT license

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ Please refer to https://github.com/Ordermind/logical-permissions-py for document
 setup(
   name = 'logical-permissions',
   version = '1.1.1',
+  license = 'MIT',
   description = 'Provides support for dictionary-based permissions with logic gates such as AND and OR.',
   long_description = long_description,
   url = 'https://github.com/Ordermind/logical-permissions-py',
