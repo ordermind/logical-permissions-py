6eaee763e07ed0f6fc667f211857efdd8743fbc4
Kristofer Tengstrom <info@kristofer-tengstrom.se>
2016-12-23 19:33:30 +0100

    kristofer: Changed version to 1.2.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ Please refer to https://github.com/Ordermind/logical-permissions-py for document
 
 setup(
   name = 'logical-permissions',
-  version = '1.1.2',
+  version = '1.2.0',
   license = 'MIT',
   description = 'Provides support for dictionary-based permissions with logic gates such as AND and OR.',
   long_description = long_description,
